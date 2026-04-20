2742ca6e1a14b244ecec44fba7ddb76d9d1135bf
Oskar Wieczorek <planacyblack@gmail.com>
2026-04-20 19:38:57 +0200

    Ćwiczenie 3 - wprowadzenie metodyk ,narazie bez ruszania main

---
+++ APBD-PGO-Cw3-s34954/src/ProduktMenu.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+
+    import java.util.Objects;
+
+    public class ProduktMenu {
+        private String kod;
+        private String nazwa;
+        private double cena;
+        private String kategoria;
+        private static int liczbaProduktow = 0;
+
+        public ProduktMenu(String kod, String nazwa, double cena, String kategoria) {
+            this.kod = kod;
+            this.nazwa = nazwa;
+            this.cena = cena;
+            this.kategoria = kategoria;
+            liczbaProduktow++;
+        }
+
+        public String getKod() {
+            return kod;
+        }
+
+        public double getCena() {
+            return cena;
+        }
+
+        public static int getLiczbaProduktow() {
+            return liczbaProduktow;
+        }
+
+        @Override
+        public String toString() {
+            return "Produkt: " + nazwa +
+                    " | Kod: " + kod +
+                    " | Cena: " + cena +
+                    " | Kategoria: " + kategoria;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ProduktMenu)) return false;
+            ProduktMenu that = (ProduktMenu) o;
+            return Objects.equals(kod, that.kod);
+        }
+    }
+
